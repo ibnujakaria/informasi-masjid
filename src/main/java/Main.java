61b0c04e6da42c9a2b43a92f1ade12e48ec5dcea
Nurul Huda <hudasanca@gmail.com>
2017-03-14 21:03:02 +0700

    mengimport jfoenix, dan membuat scene javafx

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,28 @@
 import database.DB;
 import database.schemas.UserTableSchema;
+import javafx.application.Application;
+import javafx.scene.Group;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
 
 /**
  * Created by ibnujakaria on 3/10/17.
  */
-public class Main {
+public class Main extends Application {
+
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Group root = new Group();
+        Scene scene = new Scene(root, 600, 300);
+
+        primaryStage.setTitle("Info Masjid");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
 
     public static void main(String[] args) {
         DB.start();
-        System.out.println("Hello gradle!");
+        launch(args);
     }
 }
