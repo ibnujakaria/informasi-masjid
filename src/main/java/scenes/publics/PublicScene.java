6e7c72adc03a2ecbbdd058883589aa57acc75dbe
basith djunaedi <basithdjunaedi@gmail.com>
2017-03-15 10:26:39 +0700

    back button

--- src/main/java/scenes/publics/PublicScene.java
+++ src/main/java/scenes/publics/PublicScene.java
@@ -1,6 +1,8 @@
 package scenes.publics;
 
+import com.jfoenix.controls.JFXButton;
 import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
 import scenes.MyGroup;
 import scenes.MyScene;
 
@@ -8,16 +10,23 @@ import scenes.MyScene;
  * Created by ibnujakaria on 3/15/17.
  */
 public class PublicScene extends MyGroup {
+    JFXButton backButton;
+
     @Override
     protected void prepareLayout() {
         view = new MyScene(this, 800, 600);
+        VBox vBox = new VBox();
         Label label = new Label("Hello GUYS! INI PUBLIC SCENE");
+        backButton = new JFXButton("Back");
 
-        getChildren().add(label);
+        vBox.getChildren().addAll(label, backButton);
+        getChildren().add(vBox);
     }
 
     @Override
     protected void addListeners() {
-
+        backButton.setOnAction(event -> {
+            movePreviousScene();
+        });
     }
 }
