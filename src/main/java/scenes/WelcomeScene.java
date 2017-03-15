782882accc89bbb1f5f2a55dc8626c760d482288
basith djunaedi <basithdjunaedi@gmail.com>
2017-03-15 10:02:37 +0700

    welcome screen

--- src/main/java/scenes/WelcomeScene.java
+++ src/main/java/scenes/WelcomeScene.java
@@ -1,23 +1,56 @@
 package scenes;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.VBox;
+import javafx.scene.paint.Color;
 import javafx.scene.text.Font;
+
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 /**
  * Created by ibnujakaria on 3/15/17.
  */
 public class WelcomeScene extends MyGroup {
+    VBox vBox;
+    Label welcomeLabel;
+    ObservableList list;
+    Image logo;
+    ImageView imageView;
 
     @Override
     protected void prepareLayout() {
-        view = new MyScene(this, 800, 600);
-        Label welcomeLabel = new Label("Informasi Masjid");
-        Label loading = new Label("Loading...");
+        view = new MyScene(this, 800, 600, Color.BEIGE);
+        welcomeLabel = new Label("Informasi Masjid");
+        welcomeLabel.setFont(Font.font("FZXiHeiI-Z08",33));
+        try {
+            logo = new Image(new FileInputStream("/home/basithdj/IdeaProjects/informasi-masjid/dist/images/logo/Logo.png"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
 
-        welcomeLabel.setFont(new Font(30));
-        getChildren().addAll(welcomeLabel, loading);
+        imageView = new ImageView(logo);
+
+        imageView.setFitHeight(250);
+        imageView.setFitWidth(250);
+        imageView.setPreserveRatio(true);
+
+        vBox = new VBox();
+        vBox.setSpacing(10);
+        vBox.setMargin(imageView, new Insets(20,100,20,300));
+        vBox.setMargin(welcomeLabel, new Insets(20,100,20,300));
+
+        list = vBox.getChildren();
+        list.addAll(imageView,welcomeLabel);
+
+        getChildren().addAll(vBox);
     }
 
     @Override
