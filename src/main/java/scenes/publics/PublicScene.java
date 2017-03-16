df7901e980ab1e0131bd2a345af5a37cc1d91434
basith djunaedi <basithdjunaedi@gmail.com>
2017-03-16 14:49:04 +0700

    nambah css

--- src/main/java/scenes/publics/PublicScene.java
+++ src/main/java/scenes/publics/PublicScene.java
@@ -1,33 +1,67 @@
 package scenes.publics;
 
 import com.jfoenix.controls.JFXButton;
+import javafx.collections.ObservableList;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.layout.GridPane;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
+import javafx.scene.paint.ImagePattern;
 import javafx.scene.text.Font;
 import scenes.MyGroup;
 import scenes.MyScene;
 import scenes.publics.auth.LoginScene;
 
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.nio.file.Paths;
+
 /**
  * Created by ibnujakaria on 3/15/17.
  */
 public class PublicScene extends MyGroup {
+    VBox vBox;
     JFXButton backButton, loginButton, tanyaButton;
-
+    Image background;
     @Override
     protected void prepareLayout() {
         view = new MyScene(this, 800, 600);
-        VBox vBox = new VBox();
+        vBox = new VBox();
+
+        String uri = Paths.get("dist/css/welcome.css").toUri().toString();
+        view.getStylesheets().add(uri);
+        try {
+            background = new Image(new FileInputStream("dist/images/logo/blur.jpg"));
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        }
+
+        ImagePattern imagePattern = new ImagePattern(background);
+        view.setFill(imagePattern);
+
         Label label = new Label("PUBLIC SCENE");
         label.setFont(new Font(30));
         backButton = new JFXButton("Back");
         loginButton = new JFXButton("Masuk");
         tanyaButton = new JFXButton("Tanya");
 
-        HBox buttons = new HBox();
-        buttons.getChildren().addAll(loginButton, tanyaButton);
-        vBox.getChildren().addAll(backButton, label, buttons);
+
+        GridPane buttons = new GridPane();
+        buttons.setPadding(new Insets(10, 10, 10, 10));
+        buttons.setVgap(5);
+        buttons.setHgap(5);
+        buttons.add(loginButton, 0, 2);
+        buttons.add(tanyaButton, 1, 2);
+        setvBoxFullScreen();
+        vBox.setAlignment(Pos.CENTER);
+        vBox.setMargin(buttons, new Insets(20, 20, 20, 320));
+        ObservableList list = vBox.getChildren();
+
+        //Adding all the nodes to the observable list
+        list.addAll(backButton,label,buttons);
         getChildren().add(vBox);
     }
 
@@ -41,5 +75,11 @@ public class PublicScene extends MyGroup {
             setNextScene(new LoginScene());
             moveNextScene();
         });
+
+    }
+
+    private void setvBoxFullScreen () {
+        vBox.setMinHeight(view.getHeight());
+        vBox.setMinWidth(view.getWidth());
     }
 }
