baf30d77ca44a70c7da78222cdea871131538c02
Nurul Huda <hudasanca@gmail.com>
2017-03-16 21:38:16 +0700

    login process done

--- src/main/java/scenes/publics/auth/LoginScene.java
+++ src/main/java/scenes/publics/auth/LoginScene.java
@@ -3,12 +3,14 @@ package scenes.publics.auth;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXPasswordField;
 import com.jfoenix.controls.JFXTextField;
+import core.auth.Auth;
 import javafx.scene.control.Label;
 import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import javafx.scene.text.Font;
 import scenes.MyGroup;
 import scenes.MyScene;
+import scenes.dashboard.DashboardScene;
 import scenes.publics.PublicScene;
 
 /**
@@ -18,6 +20,7 @@ public class LoginScene extends MyGroup {
     JFXTextField usernameField;
     JFXPasswordField passwordField;
     JFXButton loginButton, signUpButton, backButton;
+    Label errorLabel;
 
     @Override
     protected void prepareLayout() {
@@ -36,10 +39,13 @@ public class LoginScene extends MyGroup {
         loginButton = new JFXButton("Login");
         signUpButton = new JFXButton("Sign up");
 
+        errorLabel = new Label("Login gagal");
+        errorLabel.setVisible(false);
+
         HBox buttons = new HBox();
         buttons.getChildren().addAll(loginButton, signUpButton);
 
-        vBox.getChildren().addAll(backButton, header, usernameField, passwordField, buttons);
+        vBox.getChildren().addAll(backButton, header, usernameField, passwordField, errorLabel, buttons);
         getChildren().add(vBox);
     }
 
@@ -49,10 +55,26 @@ public class LoginScene extends MyGroup {
             movePreviousScene();
         });
 
+        loginButton.setOnAction(event -> {
+            doLoginProcess();
+        });
+
         signUpButton.setOnAction(event -> {
             PublicScene publicScene = (PublicScene) getPreviousScene();
             publicScene.setNextScene(new SignupScene());
             publicScene.moveNextScene();
         });
+    }
+
+    private void doLoginProcess() {
+        boolean success = Auth.attemps(usernameField.getText(), passwordField.getText());
+        errorLabel.setVisible(false);
+        
+        if (success) {
+            setNextScene(new DashboardScene());
+            moveNextScene();
+        } else {
+            errorLabel.setVisible(true);
+        }
     }
 }
