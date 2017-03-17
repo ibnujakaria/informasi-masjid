d37c063fdb272cb778b9d38c79a77b1d58d0ec27
abdulloh <wahid4bdullah@gmail.com>
2017-03-17 15:39:54 +0700

    slice topMenu on publicScene into pieces

--- src/main/java/scenes/publics/PublicScene.java
+++ src/main/java/scenes/publics/PublicScene.java
@@ -3,22 +3,19 @@ package scenes.publics;
 import app.Main;
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXListView;
+import com.jfoenix.controls.JFXPopup;
 import com.jfoenix.controls.JFXTabPane;
 import javafx.collections.ObservableList;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
-import javafx.scene.control.Label;
-import javafx.scene.control.Tab;
-import javafx.scene.control.TabPane;
+import javafx.scene.control.*;
 import javafx.scene.image.Image;
-import javafx.scene.layout.BorderPane;
-import javafx.scene.layout.GridPane;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
+import javafx.scene.layout.*;
 import javafx.scene.paint.ImagePattern;
 import javafx.scene.text.Font;
 import scenes.MyGroup;
 import scenes.MyScene;
+import scenes.pieces.TopMenu;
 import scenes.publics.auth.LoginScene;
 
 import java.io.FileInputStream;
@@ -29,7 +26,7 @@ import java.nio.file.Paths;
  * Created by ibnujakaria on 3/15/17.
  */
 public class PublicScene extends MyGroup {
-    VBox vBox;
+//    VBox vBox;
     JFXButton backButton, loginButton, tanyaButton;
     Image background;
     JFXTabPane tabPane;
@@ -38,7 +35,10 @@ public class PublicScene extends MyGroup {
     JFXListView<Label> listSchedule;
     @Override
     protected void prepareLayout() {
-        vBox = new VBox();
+        String uri = Paths.get("dist/css/welcome.css").toUri().toString();
+        Main.primaryStage.getScene().getStylesheets().add(uri);
+
+//        vBox = new VBox();
         tabPane = new JFXTabPane();
         listSchedule = new JFXListView<Label>();
         for(int i = 0 ; i < 4 ; i++) listSchedule.getItems().add(new Label("Item " + i));
@@ -51,19 +51,14 @@ public class PublicScene extends MyGroup {
         tab2.setText("Tanya Ustadz");
         tab3.setText("Rekaman Kajian");
 
-        double max = Main.primaryStage.getScene().getWidth() /3;
-        tabPane.setTabMinWidth(max);
-        tabPane.setTabMinHeight(70);
         tabPane.getTabs().addAll(tab1,tab2,tab3);
 
         // bind to take available space
         borderPane.prefHeightProperty().bind(Main.primaryStage.getScene().heightProperty());
         borderPane.prefWidthProperty().bind(Main.primaryStage.getScene().widthProperty());
-
+        borderPane.setTop(new TopMenu());
         borderPane.setCenter(tabPane);
 
-        String uri = Paths.get("dist/css/welcome.css").toUri().toString();
-        Main.primaryStage.getScene().getStylesheets().add(uri);
 
         try {
             background = new Image(new FileInputStream("dist/images/logo/blur.jpg"));
@@ -74,9 +69,10 @@ public class PublicScene extends MyGroup {
         ImagePattern imagePattern = new ImagePattern(background);
         Main.primaryStage.getScene().setFill(imagePattern);
 
-        Label label = new Label("PUBLIC SCENE");
-        label.setFont(new Font(30));
-        backButton = new JFXButton("Back");
+//        gak kepakek
+//        Label label = new Label("PUBLIC SCENE");
+//        label.setFont(new Font(30));
+//        backButton = new JFXButton("Back");
         loginButton = new JFXButton("Masuk");
         tanyaButton = new JFXButton("Tanya");
 
@@ -87,22 +83,22 @@ public class PublicScene extends MyGroup {
         buttons.setHgap(5);
         buttons.add(loginButton, 0, 2);
         buttons.add(tanyaButton, 1, 2);
-        setvBoxFullScreen();
-        vBox.setMargin(buttons, new Insets(20, 20, 20, 320));
-        ObservableList list = vBox.getChildren();
+//        setvBoxFullScreen();
+//        vBox.setMargin(buttons, new Insets(20, 20, 20, 320));
+//        ObservableList list = vBox.getChildren();
         tab1.setContent(listSchedule);
         tab2.setContent(buttons);
         tab3.setContent(new Label("Content 3"));
         //Adding all the nodes to the observable list
-        list.addAll(borderPane);
-        getChildren().add(vBox);
+//        list.addAll(borderPane);
+        getChildren().add(borderPane);
     }
 
     @Override
     protected void addListeners() {
-        backButton.setOnAction(event -> {
-            movePreviousScene();
-        });
+//        backButton.setOnAction(event -> {
+//            movePreviousScene();
+//        });
 
         loginButton.setOnAction(event -> {
             setNextScene(new LoginScene());
@@ -114,17 +110,17 @@ public class PublicScene extends MyGroup {
     @Override
     protected void onAfterNext() {
         super.onAfterNext();
-        setvBoxFullScreen();
+//        setvBoxFullScreen();
     }
 
     @Override
     protected void onAfterBack() {
         super.onAfterBack();
-        setvBoxFullScreen();
+//        setvBoxFullScreen();
     }
 
     private void setvBoxFullScreen () {
-        vBox.setMinHeight(Main.primaryStage.getScene().getHeight());
-        vBox.setMinWidth(Main.primaryStage.getScene().getWidth());
+//        vBox.setMinHeight(Main.primaryStage.getScene().getHeight());
+//        vBox.setMinWidth(Main.primaryStage.getScene().getWidth());
     }
 }
