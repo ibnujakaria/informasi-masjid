61c315f53cd683f69d9028703d0352d98abfbc19
abdulloh <wahid4bdullah@gmail.com>
2017-03-19 16:53:42 +0700

    change dashboard scene view, still unfix

--- src/main/java/scenes/pieces/PertanyaanContent.java
+++ src/main/java/scenes/pieces/PertanyaanContent.java
@@ -13,6 +13,7 @@ import javafx.scene.layout.Priority;
 import javafx.scene.layout.StackPane;
 import javafx.scene.layout.VBox;
 import org.jooq.Record;
+import org.jooq.Result;
 import scenes.MyGroup;
 import scenes.publics.PublicScene;
 import scenes.publics.auth.LoginScene;
@@ -21,30 +22,34 @@ import scenes.publics.auth.LoginScene;
  * Created by abdullah on 17/03/17.
  */
 public class PertanyaanContent extends VBox {
-//    JFXButton tanyaButton;
+    JFXButton tanyaButton;
     JFXListView<VBox> listPertanyaan;
-//    StackPane askPane;
+    StackPane askPane;
 
-    public PertanyaanContent(MyGroup myGroup) {
+    public PertanyaanContent(MyGroup myGroup, Result<Record> questions) {
+        prepareLayout(myGroup, questions);
+
+        if (Auth.isLogin() && !Auth.isUstadz() && !Auth.isAdmin()) addAskButton();
+    }
+
+    public void prepareLayout(MyGroup myGroup, Result<Record> questions) {
         listPertanyaan = new JFXListView<>();
-        for (Record question : Question.getAnsweredQuestion()) {
+        for (Record question : questions) {
             listPertanyaan.getItems().add(new VBox(new QuestionComponent(question, myGroup)));
         }
 
         getChildren().add(listPertanyaan);
-
-//        if (Auth.isLogin()) addAskButton();
     }
 
-//    private void addAskButton() {
-//        askPane = new StackPane();
-//        tanyaButton = new JFXButton("Tanya Sesuatu");
-//        askPane.getChildren().add(tanyaButton);
-//        askPane.setAlignment(Pos.BOTTOM_RIGHT);
-//        askPane.setMargin(tanyaButton, new Insets(0, 10, 10, 0));
-//        setVgrow(askPane, Priority.ALWAYS);
-//        getChildren().add(askPane);
-//    }
+    private void addAskButton() {
+        askPane = new StackPane();
+        tanyaButton = new JFXButton("Tanya Sesuatu");
+        askPane.getChildren().add(tanyaButton);
+        askPane.setAlignment(Pos.BOTTOM_RIGHT);
+        askPane.setMargin(tanyaButton, new Insets(0, 10, 10, 0));
+        setVgrow(askPane, Priority.ALWAYS);
+        getChildren().add(askPane);
+    }
 //
 //    private void addListener(JFXButton button) {
 //        button.setOnAction(event -> {
