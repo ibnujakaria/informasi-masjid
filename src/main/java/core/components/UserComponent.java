362d179dcb80bc36a4ac9e9cc2ebf38e128a2938
Nurul Huda <hudasanca@gmail.com>
2017-03-19 14:42:30 +0700

    menambah tombol edit/hapus di users, dan ustadz

--- src/main/java/core/components/UserComponent.java
+++ src/main/java/core/components/UserComponent.java
@@ -1,7 +1,10 @@
 package core.components;
 
+import com.jfoenix.controls.JFXButton;
+import core.auth.Auth;
 import javafx.scene.Group;
 import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
 import javafx.scene.layout.VBox;
 import org.jooq.Record;
 
@@ -13,6 +16,7 @@ public class UserComponent extends Group {
     private Record user;
     private VBox vBox;
     private Label nameLabel, emailLabel, usernameLabel, cityLabel;
+    private JFXButton editButton, deleteButton;
 
     public UserComponent (Record user) {
         this.user = user;
@@ -28,7 +32,15 @@ public class UserComponent extends Group {
         emailLabel = new Label(user.get("email").toString());
         cityLabel = new Label(user.get("address") != null ? user.get("address").toString() : "");
 
-        vBox.getChildren().addAll(nameLabel, usernameLabel, emailLabel, cityLabel);
+        editButton = new JFXButton("Edit");
+        deleteButton = new JFXButton("Hapus");
+
+        editButton.setVisible(Auth.isLogin() && Auth.isAdmin());
+        deleteButton.setVisible(Auth.isLogin() && Auth.isAdmin());
+
+        vBox.getChildren().addAll(nameLabel, usernameLabel, emailLabel,
+                cityLabel, new HBox(editButton, deleteButton));
+
         getChildren().addAll(vBox);
     }
 }
