c54807527957b695d041e01048ee57d9c45c2791
basith djunaedi <basithdjunaedi@gmail.com>
2017-03-19 14:32:41 +0700

    back button is back

--- src/main/java/scenes/publics/PublicScene.java
+++ src/main/java/scenes/publics/PublicScene.java
@@ -4,20 +4,14 @@ import app.Main;
 import com.jfoenix.controls.*;
 import javafx.beans.value.ChangeListener;
 import javafx.beans.value.ObservableValue;
-import javafx.collections.ObservableList;
-import javafx.geometry.Insets;
-import javafx.geometry.Pos;
 import javafx.scene.control.*;
 import javafx.scene.image.Image;
 import javafx.scene.layout.*;
 import javafx.scene.paint.ImagePattern;
-import javafx.scene.text.Font;
 import scenes.MyGroup;
-import scenes.MyScene;
 import scenes.pieces.JadwalContent;
 import scenes.pieces.PertanyaanContent;
 import scenes.pieces.TopMenu;
-import scenes.publics.auth.LoginScene;
 
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
@@ -27,8 +21,6 @@ import java.nio.file.Paths;
  * Created by ibnujakaria on 3/15/17.
  */
 public class PublicScene extends MyGroup {
-//    VBox vBox;
-//    JFXButton backButton, loginButton, tanyaButton;
     Image background;
     JFXTabPane tabPane;
     BorderPane borderPane;
@@ -38,12 +30,7 @@ public class PublicScene extends MyGroup {
     protected void prepareLayout() {
         String uri = Paths.get("dist/css/public-scene.css").toUri().toString();
         Main.primaryStage.getScene().getStylesheets().add(uri);
-
-//        vBox = new VBox();
         tabPane = new JFXTabPane();
-//        listSchedule = new JFXListView<Label>();
-//        for(int i = 0 ; i < 4 ; i++) listSchedule.getItems().add(new Label("Item " + i));
-
         borderPane = new BorderPane();
         tab1 = new Tab();
         tab2 = new Tab();
@@ -51,10 +38,8 @@ public class PublicScene extends MyGroup {
         tab1.setText("Jadwal Kajian");
         tab2.setText("Tanya Ustadz");
         tab3.setText("Rekaman Kajian");
-
         tabPane.getTabs().addAll(tab1,tab2,tab3);
 
-        // bind to take available space
         borderPane.prefHeightProperty().bind(Main.primaryStage.getScene().heightProperty());
         borderPane.prefWidthProperty().bind(Main.primaryStage.getScene().widthProperty());
         borderPane.setTop(new TopMenu());
@@ -68,29 +53,9 @@ public class PublicScene extends MyGroup {
 
         ImagePattern imagePattern = new ImagePattern(background);
         Main.primaryStage.getScene().setFill(imagePattern);
-
-//        gak kepakek
-//        Label label = new Label("PUBLIC SCENE");
-//        label.setFont(new Font(30));
-//        backButton = new JFXButton("Back");
-//        loginButton = new JFXButton("Masuk");
-//        tanyaButton = new JFXButton("Tanya");
-
-
-//        GridPane buttons = new GridPane();
-//        buttons.setPadding(new Insets(10, 10, 10, 10));
-//        buttons.setVgap(5);
-//        buttons.setHgap(5);
-//        buttons.add(loginButton, 0, 2);
-//        buttons.add(tanyaButton, 1, 2);
-//        setvBoxFullScreen();
-//        vBox.setMargin(buttons, new Insets(20, 20, 20, 320));
-//        ObservableList list = vBox.getChildren();
         tab1.setContent(new JadwalContent());
         tab2.setContent(new PertanyaanContent(this));
         tab3.setContent(new Label("Content 3"));
-        //Adding all the nodes to the observable list
-//        list.addAll(borderPane);
         getChildren().add(borderPane);
     }
 
@@ -112,15 +77,6 @@ public class PublicScene extends MyGroup {
                 setvBoxFullScreen();
             }
         });
-//        backButton.setOnAction(event -> {
-//            movePreviousScene();
-//        });
-
-//        loginButton.setOnAction(event -> {
-//            setNextScene(new LoginScene());
-//            moveNextScene();
-//        });
-
     }
 
     @Override
