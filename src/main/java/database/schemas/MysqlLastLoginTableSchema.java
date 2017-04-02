c755f4946e7dda5e611b331a062da0fb25302a3a
Nurul Huda <hudasanca@gmail.com>
2017-04-02 17:41:53 +0700

    oops, typo

--- src/main/java/database/schemas/MysqlLastLoginTableSchema.java
+++ src/main/java/database/schemas/MysqlLastLoginTableSchema.java
@@ -3,7 +3,7 @@ package database.schemas;
 /**
  * Created by ibnujakaria on 20/03/17.
  */
-public dd class MysqlLastLoginTableSchema extends MysqlSchema{
+public class MysqlLastLoginTableSchema extends MysqlSchema{
     @Override
     protected int getVersion() {
         return 2;
